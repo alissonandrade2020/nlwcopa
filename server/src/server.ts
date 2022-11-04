544a0ce8e314365e5fd0d59ed8a5c7ed15da087b
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2022-11-04 11:04:44 -0300

    Aula 04
    Aula 04

--- server/src/server.ts
+++ server/src/server.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   await fastify.register(guessRoutes)
   await fastify.register(userRoutes)
 
-  await fastify.listen({ port: 3333, /* host: '0.0.0.0' */ })
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
