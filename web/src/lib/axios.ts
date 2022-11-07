7026663ab780d8590864802723287209a3a77ebd
alisson de andrade <alisson2d@hotmail.com>
2022-11-07 13:11:24 -0300

    Conexão back e front e mobile

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
   // baseURL: 'http://localhost:3333',
-  baseURL: 'https://nlwcopa-production.up.railway.app/'
+  baseURL: 'https://nlwcopa-production.up.railway.app/',
 });
