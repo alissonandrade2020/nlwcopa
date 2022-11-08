3ed1f509a4cbc1b5ea22bee9d896a894941158bf
alissonandrade2020 <alisson2d@hotmail.com>
2022-11-07 22:41:31 -0300

    Ok

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.3:3333',
-  // baseURL: 'http://nlwcopa-production.up.railway.app'
+  // baseURL: 'http://192.168.1.3:3333',
+  baseURL: 'http://nlwcopa-production.up.railway.app'
 })
